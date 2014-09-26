524cfea2518b7f6852a486f1dde87ab0f46ca700
innerpeace <anjicun@live.com>
2014-09-26 17:00:02 +0800

    添加了管理员登录窗口

--- app/controllers/manage/session_controller.rb
+++ app/controllers/manage/session_controller.rb
@@ -3,5 +3,13 @@ class Manage::SessionController < ApplicationController
   end
 
   def create
+  	if manage_admin = Manage::Admin.auth(params[:name],params[:password])
+			session[:user_id]=user.id
+			session[:user_name]=user.name
+			session[:user_is_admin]=user.is_admin
+			redirect_to root
+		else
+			redirect_to manage_login_url,:alert => "不正确的用户名/密码"
+		end
   end
 end
