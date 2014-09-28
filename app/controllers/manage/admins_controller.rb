e0e51c44356c9b2465b818461e91da4cb704a3f7
Summit <1420646999@qq.com>
2014-09-28 22:35:50 +0800

    修改了admin的控制器和视图

--- app/controllers/manage/admins_controller.rb
+++ app/controllers/manage/admins_controller.rb
@@ -20,33 +20,8 @@ class Manage::AdminsController < ManageController
 
   # GET /manage/admins/1/edit
   def edit
-  end
-
-   def edit_role
-      @admin_roles= Manage::Admin.find(params[:admin_id]).roles
+      @admin_roles= Manage::Admin.find(params[:id]).roles
       @manage_roles=Manage::Role.all
-  end
-
-  def update_role
-       roles_id=params[:roles]
-       admin_roles= Manage::Admin.find(params[:admin_id]).roles
-       admin_roles.clear
-       if roles_id
-           roles_id.each do |role_id|
-       	  role=Manage::Role.find(role_id)
-       	  if role_id==nil
-       	       admin_roles.clear
-       	       respond_to do |format|
-                          format.html { redirect_to manage_admin_edit_path(params[:admin_id]), notice: '角色不存在,修改失败' }
-                      end
-                 else
-       	       admin_roles<<role
-                 end
-           end
-         end
-          respond_to do |format|
-              format.html { redirect_to manage_admin_path(params[:admin_id]), notice: '角色修改成功' }
-          end
   end
   # POST /manage/admins
   # POST /manage/admins.json
@@ -67,6 +42,22 @@ class Manage::AdminsController < ManageController
   # PATCH/PUT /manage/admins/1
   # PATCH/PUT /manage/admins/1.json
   def update
+     roles_id=params[:roles]
+     admin_roles= Manage::Admin.find(params[:id]).roles
+     admin_roles.clear
+     if roles_id
+         roles_id.each do |role_id|
+       	role=Manage::Role.find(role_id)
+       	if role_id==nil
+       	     admin_roles.clear
+       	     respond_to do |format|
+                        format.html { redirect_to manage_admin_edit_path(params[:admin_id]), notice: '角色不存在,修改失败' }
+                    end
+               else
+                  admin_roles<<role
+              end
+         end
+    end
     respond_to do |format|
       if @manage_admin.update(manage_admin_params)
         format.html { redirect_to @manage_admin, notice: 'Admin was successfully updated.' }
