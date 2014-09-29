2eb79d376a96fa756329e24effe78494455055f2
filow <lirenhai427@gmail.com>
2014-09-29 16:41:43 +0800

    添加了对config模型的field_type字段的取值限制

--- app/models/manage/config.rb
+++ app/models/manage/config.rb
@@ -1,4 +1,19 @@
 class Manage::Config < ActiveRecord::Base
     belongs_to :config_type
-
+    validates_inclusion_of :field_type,in: %w(text textarea rich_text datetime_picker email url number array select multiple_select),allow_nil:true,allow_empty:true
+    
+    def self.field_reflection
+        {
+            text: "text_field",
+            textarea: "text_area",
+            rich_text: "text_area",  # 之后改成富文本编辑器
+            datetime_picker: "datetime_field",
+            email: "email_field",
+            url: "url_field",
+            select: "text_field",  # 之后改成下拉菜单
+            multiple_select: "text_field",  # 之后改成多选
+            number: "number_field",
+            array: "text_field", # 后续再改
+        }
+    end
 end
