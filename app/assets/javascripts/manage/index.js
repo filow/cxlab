5d301ad2671a685286f957a93fffd24ffab91b9b
chen9434 <564936642@qq.com>
2014-09-29 13:44:44 +0800

    增加了响应式设计,完善隐藏sidebar的按钮功能

--- app/assets/javascripts/manage/index.js
+++ app/assets/javascripts/manage/index.js
@@ -1,32 +1,55 @@
 $(document).ready(function(){
 
+	//sidebar 适应屏幕
+	$(".sidebar").height($(window.height) - $(".navbar").height());
+	
 	//iframe 自适应	
-	var iframe = $('#main_frame');
-	var sidebar_width = $('.sidebar').width();
-	var navbar_height = $('.navbar').height();
-	var frame_height = $(window).height() - navbar_height-5;
-	var frame_width = $(window).width() - sidebar_width;
-	iframe.height(frame_height);
-	iframe.width(frame_width);
-	$(window).resize(function(data){
-		frame_height = $(window).height() - navbar_height-5;
-		frame_width = $(window).width() - sidebar_width;		
+	function iframeAutoAdapt() 
+	{
+		var iframe = $('#main_frame');
+		var sidebar_width;
+		if($(".sidebar").css("display") == "block") 
+			sidebar_width = $('.sidebar').width();
+		else 
+			sidebar_width = 0;
+		var navbar_height = $('.navbar').height();
+		var frame_height = $(window).height() - navbar_height - 5;
+		var frame_width;
+		if($(window).width()>760)
+			frame_width = $(window).width() - sidebar_width;
+		else 
+			frame_width = $(window).width();
 		iframe.height(frame_height);
-		iframe.width(frame_width);
-		console.log(frame_height+","+frame_width);
+		iframe.width(frame_width);	
+	}
+	iframeAutoAdapt();
+	$(window).resize(function(data){
+		iframeAutoAdapt();
+		bodyAutoAdapt();
 	});
 	
-	var collapsed = false;
- 	$("#collapsed-min").click(function(){
- 		$(".sidebar").toggle();
- 		if(!collapsed){
- 			collapsed = true;
+	//隐藏sidebar按钮
+	var collapsed;
+	function bodyAutoAdapt() //body适应sidebar的有无
+	{
+		var hide = $(".sidebar").css("display");
+		if(hide == "block") collapsed = false;
+		else collapsed = true;
+ 		if(collapsed){
+ 			collapsed = false;
  			$(".body").css("margin-left","0");
  		}
  		else{
- 			collapsed = false;
+ 			collapsed = true;
  			$(".body").css("margin-left","225px"); 			
  		}
+	}
+	bodyAutoAdapt();
+ 	$("#collapsed-min").click(function(){
+ 		$(".sidebar").toggle();
+		$(".sidebar").height($(window.height) - $(".navbar").height());
+ 		bodyAutoAdapt();
+ 		iframeAutoAdapt();
  	});
 
     //sidebar 滚动条初始化
