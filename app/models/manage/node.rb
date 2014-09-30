6874286329a6fa462f7cac37a3a08df9fd50ff8f
filow <lirenhai427@gmail.com>
2014-09-30 11:02:15 +0800

    添加node和role的关联，增加了tree_view函数，方便将权限组装成父-子结构

--- app/models/manage/node.rb
+++ app/models/manage/node.rb
@@ -1,2 +1,29 @@
 class Manage::Node < ActiveRecord::Base
+    validates_numericality_of :pid
+    validates_presence_of :name
+    validates_uniqueness_of :name,on: :create
+
+
+    has_and_belongs_to_many :roles
+    belongs_to :parent, class_name: "Manage::Node",foreign_key: "pid"
+
+
+    def self.tree_view
+        parent_nodes=where(pid: 0).order(sort: :asc).all
+        child_nodes=where('pid != 0').all
+        result=[]
+        parent_nodes.each do |pnode|
+            temp_node=pnode
+            temp_node.child=child_nodes.find_all{|cnode| cnode.pid==temp_node.id}.sort_by{|x| x.sort}
+            result<<temp_node
+        end
+        result
+    end
+
+    def child=(val)
+        @child=val
+    end
+    def child
+        @child
+    end
 end
