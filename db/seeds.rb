005d3b2006aa99b5d0c9915d32b5caf8a7c15e50
filow <lirenhai427@gmail.com>
2014-10-04 12:45:35 +0800

    添加了权限节点配置文件

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 加载配置文件
+
+puts "开始更新权限节点信息...."
+nodes = YAML.load(File.read("config/nodes.yml"))
+nodes.each do |key,val|
+    db_item=Manage::Node.find_by_name(key)
+    if db_item
+        print "更新记录#{db_item.name}.."
+        result=db_item.update(val)
+        if result
+            puts "成功"
+        else
+            puts "失败"
+        end
+    else
+        Manage::Node.create(val.merge({"name"=>key}))
+        puts "添加记录#{result.name}:#{result.title}"
+    end
+end
