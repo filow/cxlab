05aa143e55e11057aa0bb887ec82f6965397d6ac
JetMuffin <564936642@qq.com>
2014-10-12 23:26:15 +0800

    学生名单加入ext-grid列表，应用其编辑、新建、删除与后台的连接

--- app/assets/javascripts/manage/index.js
+++ app/assets/javascripts/manage/index.js
@@ -54,57 +54,38 @@ $(document).ready(function(){
  		iframeAutoAdapt();
  	});
 
-    //sidebar 滚动条初始化
-    $(".sidebar-container").mCustomScrollbar({
-        scrollInertia:150
-    });
+    	//sidebar 滚动条初始化
+    	$(".sidebar-container").mCustomScrollbar({
+        		scrollInertia:150
+    	});
 
-    //滚动条scroller dragger 显示处理
-    $(".sidebar-container").hover(function () {
-        $(".mCSB_dragger").fadeIn("fast");
-    },function () {
-        $(".mCSB_dragger").fadeOut("fast");
-      });
+    	//滚动条scroller dragger 显示处理
+    	$(".sidebar-container").hover(function () {
+        		$(".mCSB_dragger").fadeIn("fast");
+    	},function () {
+        		$(".mCSB_dragger").fadeOut("fast");
+      	});
 
-    //折叠二级菜单
-    // var ToggleButton = $(".toggle-button");
-    // var SubNav = $(".sidebar-subnav");
-    // $(".sidebar-nav > li.sidebar-subnav-list > .sidebar-subnav-button").each(function (i) {
-    //     $(this).click(function () {
-    //         if ($(SubNav[i]).css("display") == "block") {
-    //             $(SubNav[i]).slideUp(300);
-    //             $(ToggleButton[i]).removeClass("fa fa-chevron-down").addClass("fa fa-chevron-left");
+	//折叠二级菜单
+    	var nav = $(".sidebar-nav");
+    	var sidebar_group = $(" .sidebar-group");
+    	$(".sidebar-group > .sidebar-header-button").each(function (i) {
+        		$(this).click(function () {
+            			if ($(nav[i]).css("display") == "block") {
+                			$(nav[i]).slideUp(300);
+                			$(this).parent().removeClass('open')
+            			} else {
+	                		for (var j = 0; j < nav.length; j++) {
+	                    			$(nav[j]).slideUp(200);
+	                    			$(sidebar_group[j]).removeClass('open')
+	                		}
+	                		$(nav[i]).slideDown(200);
+	                		$(this).parent().addClass('open')
+ 	           		}
+        		});
+    	});	
 
-    //         } else {
-    //             for (var j = 0; j < SubNav.length; j++) {
-    //                 $(SubNav[j]).slideUp(300);
-    //                 $(ToggleButton[j]).removeClass("fa fa-chevron-down").addClass("fa fa-chevron-left");
-    //             }
-    //             $(SubNav[i]).slideDown(300);
-    //             $(ToggleButton[i]).removeClass("fa fa-chevron-left").addClass("fa fa-chevron-down");
-    //         }
-    //     });
-    // });
-
-    var nav = $(".sidebar-nav");
-    var sidebar_group = $(" .sidebar-group");
-    $(".sidebar-group > .sidebar-header-button").each(function (i) {
-        $(this).click(function () {
-            if ($(nav[i]).css("display") == "block") {
-                $(nav[i]).slideUp(300);
-                $(this).parent().removeClass('open')
-            } else {
-                for (var j = 0; j < nav.length; j++) {
-                    $(nav[j]).slideUp(300);
-                    $(sidebar_group[j]).removeClass('open')
-                }
-                $(nav[i]).slideDown(300);
-                $(this).parent().addClass('open')
-            }
-        });
-    });	
-
-    var OldHref,NewHrefBase,BaseHref,Module;
+    	var OldHref,NewHrefBase,BaseHref,Module;
 	//解析URL分析module
 	function module_analysis()
 	{
@@ -127,11 +108,11 @@ $(document).ready(function(){
 		    	}
 	    	});
 		$("#message").text("正在加载.."); 
-		  	$("iframe").attr("src",Module_url);
-		  	$("iframe").load(function(){ 
-		        	$("#message").text(""); 
+	  	$("iframe").attr("src",Module_url);
+	  	$("iframe").load(function(){ 
+	        		$("#message").text(""); 
 			$("iframe").removeClass("iframe");
-	    	}); 
+    		}); 
 
 	  	//删除原有的active类
 	  	$(".sidebar-group").each(function(){
