0949eacae2cbbd38c743318b9b788f8f506f1104
InnerAc <anjicun@live.com>
2014-10-12 21:44:45 +0800

    添加了登录页面的测试，修改了管理员信息的修改，额..update 还没写好，明天去实验室把书拿回来改

--- app/controllers/manage/self_controller.rb
+++ app/controllers/manage/self_controller.rb
@@ -4,5 +4,15 @@ class Manage::SelfController < ManageController
   end
 
   def update
+  	respond_to do |format|
+      if @manage_admin.update(manage_admin_params)
+
+        format.html { redirect_to @manage_admin, notice: '管理员信息更新成功.' }
+        format.json { render :show, status: :ok, location: @manage_admin }
+      else
+        format.html { render :edit }
+        format.json { render json: @manage_admin.errors, status: :unprocessable_entity }
+      end
+  	end
   end
 end
--- test/controllers/manage/session_controller_test.rb
+++ test/controllers/manage/session_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class Manage::SessionControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "访问登录页面" do
     get :index
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "用户登录" do
+  	admin = manage_admins(:admin_one)
+	post :create,  {name: admin.uid,password: '123456789'}
+ 	
+  	assert_redirected_to manage_index_index_url
+    
+  end
+  test "退出登录" do
+    delete :logout
+  	assert_redirected_to manage_login_url
   end
 
 end
