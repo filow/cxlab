44d4f4398e2efe855f4d07e74b062a6582e0fd6e
InnerAc <anjicun@live.com>
2014-10-13 17:51:56 +0800

    添加了管理员自主数据更新，在路由中添加了post /self/update

--- app/controllers/manage/self_controller.rb
+++ app/controllers/manage/self_controller.rb
@@ -2,17 +2,10 @@ class Manage::SelfController < ManageController
   def index
   	@manage_admin = Manage::Admin.find(logged_admin_id)
   end
-
+  #更新管理员数据
   def update
-  	respond_to do |format|
-      if @manage_admin.update(manage_admin_params)
-
-        format.html { redirect_to @manage_admin, notice: '管理员信息更新成功.' }
-        format.json { render :show, status: :ok, location: @manage_admin }
-      else
-        format.html { render :edit }
-        format.json { render json: @manage_admin.errors, status: :unprocessable_entity }
-      end
-  	end
+  	@manage_admin = Manage::Admin.find(logged_admin_id)
+  	Manage::Admin.update(@manage_admin.id,:uid => params[:uid],:nickname=> params[:nickname],:pwd=> params[:pwd],:email=> params[:email],:desc=> params[:desc])
+  	redirect_to manage_self_index_path
   end
 end
