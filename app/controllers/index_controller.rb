1020a2fb5dc025a73085989c914b2ca4d35ecb2c
summit <1420646999@qq.com>
2014-11-24 12:41:25 +0800

    前端用户登陆

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,13 +1,3 @@
 class IndexController < ApplicationController
-    before_filter :check_login
-
-private
-    def check_login
-        if logged_student_id
-            # 从数据库中读取用户信息
-            @student=Manage::Student.find(logged_student_id)
-        else
-            redirect_to index_login_url
-        end
-    end
+    
 end
