9b39a7a50eb0acec62600755fecb10092e647037
filow <lirenhai427@gmail.com>
2015-01-16 21:18:26 +0800

    更改资源预编译设置，减少无用文件的生成

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,27 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += [ Proc.new {|path| File.basename(path) =~ /^[^_].*\.\w+$/} ]
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/
+    # if path.split('/')[-1] =~ /^_/
+    #     false
+    # else
+    #     true
+    # end
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+    if full_path.starts_with?(app_assets_path) || full_path.starts_with?(vendor_assets_path)
+      puts "including asset: " + full_path
+      true
+    else
+      puts "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+end
 
 
 Rails.application.config.assets.paths << Rails.root.join("assets", "videos")
